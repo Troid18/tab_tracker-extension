3f792247c7afa91e9331b43ab8e45bff849dcdff
Musa Maphutha <mumaphujh024@student.wethinkcode.co.za>
2026-01-13 09:46:11 +0200

    Added a feature on the save input btn that stores the url to localStorage

--- index.js
+++ index.js
@@ -2,5 +2,35 @@ let myLeads = []
 const saveBtn = document.getElementById('save-btn')
 const tabBtn = document.getElementById('tab-btn')
 const delBtn = document.getElementById('del-btn')
+const inputEl = document.getElementById('input-btn')
+const ulEl = document.getElementById('ul-el')
+let getLeads = JSON.parse(localStorage.getItem('myLeads'))
+
+if (getLeads){
+    myLeads = getLeads
+    render(myLeads)
+}
 
 
+saveBtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value)
+    inputEl.value = ''
+    localStorage.setItem('myLeads', JSON.stringify(myLeads))
+    render(myLeads)
+})
+
+
+function render(leads){
+    let data = ''
+
+    for(let i = 0; i < leads.length; i++){
+        data += `<li> 
+        
+                <a href='${leads[i]}' >
+                    ${leads[i]}
+               </a>     
+            </li>`
+    }
+
+    ulEl.innerHTML = data
+}
