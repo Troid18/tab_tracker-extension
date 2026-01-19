1d4318bdb6a87d6f47e1acef0bbcb457e8a81da2
Musa Maphutha <mumaphujh024@student.wethinkcode.co.za>
2026-01-19 14:57:00 +0200

    feat(web-app): complete web dashboard with Firebase integration

--- web app/app.js
+++ web app/app.js
@@ -1,20 +1,27 @@
-let myLeads = []
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js"
+import { firebaseConfig } from './config.js'
+import {
+    getDatabase,
+    ref,
+    push,
+    onValue,
+    remove
+} from "https://www.gstatic.com/firebasejs/12.8.0/firebase-database.js"
+
+const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
+const referenceInDB = ref(database, "Leads")
+
+
 const saveBtn = document.getElementById('save-btn')
-const tabBtn = document.getElementById('tab-btn')
 const delBtn = document.getElementById('del-btn')
 const inputEl = document.getElementById('input-btn')
 const ulEl = document.getElementById('ul-el')
-let getLeads = JSON.parse(localStorage.getItem('myLeads'))
 
-if (getLeads){
-    myLeads = getLeads
-    render(myLeads)
-}
-
-function render(leads){
+function render(leads) {
     let data = ''
 
-    for(let i = 0; i < leads.length; i++){
+    for (let i = 0; i < leads.length; i++) {
         data += `<li> 
         
                 <a href='${leads[i]}' target= "_blank" >
@@ -27,28 +34,26 @@ function render(leads){
 }
 
 
+saveBtn.addEventListener("click", function () {
+    push(referenceInDB, inputEl.value)
+    inputEl.value = ""
+})
 
+onValue(referenceInDB, function (snapshot) {
+    const snapExists = snapshot.exists()
 
-saveBtn.addEventListener("click", function(){
-    myLeads.push(inputEl.value)
-    inputEl.value = ''
-    localStorage.setItem('myLeads', JSON.stringify(myLeads))
-    render(myLeads)
+    if (snapExists) {
+        const data = snapshot.val()
+        const value = Object.values(data)
+        render(value)
+
+    }
 })
 
 
-delBtn.addEventListener('dblclick', function(){
-    localStorage.clear()
-    myLeads = []
-    render(myLeads)
+delBtn.addEventListener('dblclick', function () {
+    remove(referenceInDB)
+    ulEl.innerHTML = ""
+
 })
 
-
-tabBtn.addEventListener('click', function(){
-    chrome.tabs.query({"active": true, "lastFocusedWindow": true}, function (tabs) {
-
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads))
-        render(myLeads)
-});
-})
---
+++ web app/config.js
@@ -0,0 +1,3 @@
+export const firebaseConfig = {
+    databaseURL: "https://tab-tracker-app-a3c14-default-rtdb.europe-west1.firebasedatabase.app/"
+}
