6f612787054f8d30c0490c088a01b41bb3e0d14a
Musa Maphutha <mumaphujh024@student.wethinkcode.co.za>
2026-01-19 10:51:35 +0200

    refactor: restructure project into a monorepo for extension and web app

---
+++ web app/app.js
@@ -0,0 +1,54 @@
+let myLeads = []
+const saveBtn = document.getElementById('save-btn')
+const tabBtn = document.getElementById('tab-btn')
+const delBtn = document.getElementById('del-btn')
+const inputEl = document.getElementById('input-btn')
+const ulEl = document.getElementById('ul-el')
+let getLeads = JSON.parse(localStorage.getItem('myLeads'))
+
+if (getLeads){
+    myLeads = getLeads
+    render(myLeads)
+}
+
+function render(leads){
+    let data = ''
+
+    for(let i = 0; i < leads.length; i++){
+        data += `<li> 
+        
+                <a href='${leads[i]}' target= "_blank" >
+                    ${leads[i]}
+               </a>     
+            </li>`
+    }
+
+    ulEl.innerHTML = data
+}
+
+
+
+
+saveBtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value)
+    inputEl.value = ''
+    localStorage.setItem('myLeads', JSON.stringify(myLeads))
+    render(myLeads)
+})
+
+
+delBtn.addEventListener('dblclick', function(){
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+
+tabBtn.addEventListener('click', function(){
+    chrome.tabs.query({"active": true, "lastFocusedWindow": true}, function (tabs) {
+
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+});
+})
